6e6ab08fce70419337590542f7ef621d51514c4c
Carlos <Carlos@DESKTOP-GHFQK5E>
2021-09-04 00:27:58 -0500

    Implement login/logout

--- Vehicles.API/Helpers/IUserHelper.cs
+++ Vehicles.API/Helpers/IUserHelper.cs
@@ -19,5 +19,8 @@ namespace Vehicles.API.Helpers
 
         Task<bool> IsUserInRoleAsync(User user, string roleName);
 
+        Task<SignInResult> LoginAsync(LoginViewModel model);
+
+        Task LogoutAsync();
     }
 }
